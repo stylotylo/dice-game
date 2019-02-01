1fe6880940b876b78ee2b2a6bfacbe91cb9cf07c
Tyler Lovell <tylerlovell@gmail.com>
2019-02-01 15:36:05 -0500

    minor html/css/adjustments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 // Step 2: Randomize a dice roll (make sure it's an integer number)
+  // Start by creating a variable that hold a dice number..1,2,3..6
+
+  let diceroll = document.getElementById('diceroll')
+  diceroll.innerHTML += `<p>You rolled: three</p>`;
+  diceroll.innerHTML += `<img src="/Users/graphic/Documents/GitHub/dice-game/img/dice3.svg"></img>`;
 
 
 // Step 3: Update the user interface (document) to show the diceface (svg image)
